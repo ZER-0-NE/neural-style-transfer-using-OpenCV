617d89df135ac7b781f0e5da312913f9834e2314
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2018-08-29 00:39:48 +0530

    Neural Style Trasnfer from images

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -19,3 +19,26 @@ image = cv2.imread(args["image"])
 image = imutils.resize(image, width = 600)
 (h, w) = image.shape[:2]
 
+blob = cv2.dnn.blobFromImage(image, 1.0, (w,h), 
+		(103.939, 116.779, 123.680), swapRB = False, crop = False)
+net.setInput(blob)
+start = time.time()
+output = net.forward()
+end = time.time()
+
+#reshaping output tensor
+output = output.reshape((3, output.shape[2], output.shape[3]))
+output[0] += 103.939
+output[1] += 116.779
+output[2] += 123.680
+
+output /= 255.0
+output = output.transpose(1,2,0)
+
+# show information on how long inference took
+print("[INFO] neural style transfer took {:.4f} seconds".format(
+	end - start))
+ 
+cv2.imshow("Input", image)
+cv2.imshow("Output", output)
+cv2.waitKey(0)
